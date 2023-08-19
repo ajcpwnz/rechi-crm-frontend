23bdf7fd9c583e792f16916a4e793139939a2ee5
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-08-19 17:33:45 +0300

    Added Routes /login, /admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-// import { useState } from 'react'
-// import { Dashboard } from "@mui/icons-material"
-// import  Login from "./pages/Login"
-import  Admin from "./pages/Admin";
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/UI/Layout/Layout';
+import SignIn from './pages/Login';
 
 function App() {
   return (
     <>
-    
-    {/* < Login /> */}
-    <Admin />
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/admin" element={<Layout />} />z
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
