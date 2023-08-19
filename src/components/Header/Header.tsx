fe6f62abf0aaf9ffc7bbab18302c4adacdfee46c
Albina Anistratenko <albuska2013@gmail.com>
2023-08-19 22:19:56 +0300

    correct

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import {AppBar, Tabs, Tab, Toolbar} from '@mui/material'; 
+import {AppBar, Toolbar, Typography, Tabs, Tab} from '@mui/material'; 
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { DrawerComp } from '../drawerComp/DrawerComp';
+import { DrawerComp } from '../drawer-comp/DrawerComp';
 
 export const Header = () => {
     return (
         <>
-<React.Fragment>
-<AppBar sx={{background: "#FEFBFF"}}>
-{/* {window.innerWidth < 480 && <DrawerComp/>} */}
+ <React.Fragment>
+<AppBar sx={{background: "#FEFBFF"}}> 
           <Toolbar>
           {window.innerWidth < 480 && <DrawerComp/>}
-<span style={{color: "#005AC1", fontSize: "24px", fontWeight: 700,
-lineHeight: "40px"}}>rechi.crm</span>
-<Tabs sx={{marginLeft: "auto"}}>
-    <Tab label={<NotificationsIcon sx={{fill: "#44474F"}}/>}/>
+<Typography style={{color: "#005AC1", fontSize: "24px", fontWeight: 700,
+lineHeight: "40px"}}>rechi.crm</Typography>
+<Tabs value={0} sx={{marginLeft: "auto"}}>
+    <Tab icon={<NotificationsIcon sx={{fill: "#44474F"}}/>} value={0}/>
 </Tabs>
 </Toolbar>   
 </AppBar>
-</React.Fragment>
+</React.Fragment> 
         </>
     )
 }
