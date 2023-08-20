0171028c58cc83b8287a2ca4c9ca02a2bc32ef90
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-08-20 04:57:25 +0300

    Added card

--- src/pages/RequestPage/RequestPage.tsx
+++ src/pages/RequestPage/RequestPage.tsx
@@ -1,24 +1,36 @@
-import Box from '@mui/material/Box'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../redux/store.ts'
-import { selectSubmissions } from '../../redux/requests/sumissionsSlice.ts'
-import { getSubmissions } from '../../services/requests.ts'
+import Box from '@mui/material/Box';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/store.ts';
+import { selectSubmissions } from '../../redux/requests/sumissionsSlice.ts';
+import { getSubmissions } from '../../services/requests.ts';
 
 export const RequestPage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getSubmissions('request')(dispatch)
-  }, [dispatch])
+    getSubmissions('request')(dispatch);
+  }, [dispatch]);
 
-  const records = useSelector((state: RootState) => selectSubmissions(state, 'request'));
+  const records = useSelector((state: RootState) =>
+    selectSubmissions(state, 'request')
+  );
 
-  return <div>
-    {records.order.map(id => {
-      return <Box>
-        {JSON.stringify(records.records[id])}
-      </Box>
-    })}
-  </div>
+  return (
+    <div>
+      {records.order.map((id) => {
+        const parsedFields = JSON.parse(records.records[id].fields);
+
+        return (
+          <Box key={id}>
+            {Object.entries(parsedFields).map(([key, value]) => (
+              <Box>
+                <b>{key}</b> <i>{value as unknown as any}</i>
+              </Box>
+            ))}
+          </Box>
+        );
+      })}
+    </div>
+  );
 };
