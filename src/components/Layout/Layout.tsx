4c1b0352138957a90f269615cdc92449710cfeff
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-08-20 06:55:37 +0300

    Update

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,8 @@
-import {useState, useEffect} from 'react'; 
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import Box from '@mui/material/Box';
 import { NavList } from '../UI/nav-list/NavList';
-
-
 
 function Layout() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
@@ -14,23 +12,27 @@ function Layout() {
       setIsMobile(window.innerWidth <= 480);
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
     <>
       <Header />
-      
-      <Box sx={{paddingTop: '64px'}}>
-        <Box sx={{padding: '10px'}}>
-          <Outlet />  
-          <div style={{display: window.innerWidth > 480 ? "flex" : "block", gap: "32px"}}>
-          <NavList/>
-          <Outlet/>
+
+      <Box sx={{ paddingTop: '64px' }}>
+        <Box sx={{ padding: '10px' }}>
+          <div
+            style={{
+              display: window.innerWidth > 480 ? 'flex' : 'block',
+              gap: '32px',
+            }}
+          >
+            <NavList />
+            <Outlet />
           </div>
         </Box>
       </Box>
