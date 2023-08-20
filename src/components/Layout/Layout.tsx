d62e17cf9c795df4b3b7c0a733688c2d14d0cfd9
Denis Sholkovy <denissholkovy@gmail.com>
2023-08-20 06:47:38 +0300

    add new components

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import Box from '@mui/material/Box';
+import {RequestFormModeration} from '../../pages/RequestFormModeration/RequestFormModeration';
 
 function Layout() {
   return (
@@ -8,7 +9,8 @@ function Layout() {
       <Header />
       <Box sx={{paddingTop: '64px'}}>
         <Box sx={{padding: '10px'}}>
-          <Outlet />
+          <Outlet />  
+          <RequestFormModeration />
         </Box>
       </Box>
     </>
